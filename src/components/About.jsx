bb35628f1bc04ea620f597c8a8e479927bccda68
AkbaralievAlisher <kgkgg28@gmail.com>
2024-01-26 00:11:36 +0600

    add text About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,8 @@ import location from '../assets/images/location.svg';
 import app from '../assets/images/app.svg';
 import diamond from '../assets/images/diamond.svg';
 import internet from '../assets/images/internet.svg';
+import furniture from '../assets/images/furniture.svg';
+import lamp from '../assets/images/lamp.svg';
 import { ScrollToContext } from '../App';
 
 function About() {
@@ -85,6 +87,43 @@ function About() {
                 мебельные ручки и различные механизмы для мебели. Также <b>Decor Profi</b> имеет
                 собственное производство моек из искусственного камня
               </p>
+              <hr />
+              <p>
+                Decor Profi специализируется на предоставлении самых качественных материалов для
+                мебели и интерьера. Наша компания предлагает широкий ассортимент материалов для
+                мебели и другие инновационные решения, которые придадут вашим мебельным проектам
+                неповторимый стиль и функциональность
+              </p>
+            </div>
+          </div>
+          <div className="title">
+            <div className="circle">
+              <h3>
+                <img src={lamp} alt="diamond" />
+              </h3>
+            </div>
+            <div className="description">
+              <p>
+                Мы стремимся к тому, чтобы каждый клиент получил высококлассное обслуживание и
+                непревзойденное качество продукции. Наша команда профессионалов всегда готова помочь
+                вам в выборе и приобретении идеальных материалов для воплощения ваших мебельных идей
+                в жизнь.
+              </p>
+              <hr />
+              <p>
+                Мы приглашаем вас на увлекательное путешествие по миру стилей и текстур, чтобы
+                создать мебельные шедевры, отражающие ваш неповторимый вкус и индивидуальность.
+              </p>
+            </div>
+          </div>
+          <div className="title">
+            <div className="circle">
+              <h3>
+                <img src={furniture} alt="diamond" />
+              </h3>
+            </div>
+            <div className="description">
+              <p></p>
               <p></p>
             </div>
           </div>
