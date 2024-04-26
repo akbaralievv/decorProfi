708cc8a29a725661fee412eee5594f7b8113ce2b
AkbaralievAlisher <kgkgg28@gmail.com>
2024-04-26 18:27:53 +0600

    fixed map

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,39 +1,25 @@
-import React, { useEffect } from 'react';
-import { Loader } from '@googlemaps/js-api-loader';
+import React from 'react';
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
-const api_key = import.meta.env.VITE_REACT_GOOGLE_MAP_API_KEY;
+const api_key = import.meta.env.VITE_REACT_YANDEX_MAP_API_KEY;
 
 function Maps() {
-  useEffect(() => {
-    const apiKey = api_key;
-    const additionalOptions = {};
-
-    const loader = new Loader({
-      apiKey,
-      version: 'weekly',
-      ...additionalOptions,
-    });
-
-    loader.load().then(async () => {
-      const { Map } = await google.maps.importLibrary('maps');
-
-      const map = new Map(document.getElementById('map'), {
-        center: { lat: 41.356992, lng: 69.248448 },
-        zoom: 17,
-      });
-      const marker = new google.maps.Marker({
-        position: { lat: 41.356992, lng: 69.248448 },
-        map,
-        title: 'Рынок Джами ул. Широк 120а',
-      });
-      marker.addListener('click', () => {
-        const url = 'https://2gis.uz/tashkent/firm/70000001061157502';
-        window.open(url, '_blank');
-      });
-    });
-  }, []);
-
-  return <div id="map"></div>;
+  return (
+    <div id="map">
+      <YMaps query={{ apikey: api_key, load: 'package.full' }}>
+        <Map
+          defaultState={{
+            center: [41.357104, 69.248502],
+            zoom: 17,
+            controls: ['zoomControl', 'fullscreenControl'],
+          }}
+          width="100%"
+          height="400px">
+          <Placemark geometry={[41.357104, 69.248502]} />
+        </Map>
+      </YMaps>
+    </div>
+  );
 }
 
 export default Maps;
